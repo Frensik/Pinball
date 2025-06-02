60c5ccefd5515e433b17b5adfe64b805f1d0e65b
LAPTOP-2NG5M1DQ\goode <goode5013@gmail.com>
2025-06-02 23:39:04 +0100

    Made the flippers pivot when a or d are used respectively

--- Assets/Scripts/Flipper.cs
+++ Assets/Scripts/Flipper.cs
@@ -19,13 +19,30 @@ public class Flipper : MonoBehaviour
     public float hitStrength = 10000f;
     public float flipperDamper = 150f;
     public string inputName;
+    HingeJoint hinge;
+
+    private void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+        hinge.useSpring = true;
+    }
 
     private void Update()
     {
+        JointSpring spring = new JointSpring();
+        spring.spring = hitStrength;
+        spring.damper = flipperDamper;
+
         if(Input.GetAxis(inputName)==1)
         {
-
+            spring.targetPosition = pressedPosition;
         }
+        else
+        {
+            spring.targetPosition = restPosition;
+        }
+        hinge.spring = spring;
+        hinge.useLimits = true;
     }
 }
 
