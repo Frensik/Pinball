603220fd25e68aa451242596ddca06ff3b173274
LAPTOP-2NG5M1DQ\goode <goode5013@gmail.com>
2025-06-02 23:26:31 +0100

    Added new flippers with a hinge joint, added layers to the flippers so that they don't have to collide with the stage

--- Assets/Scripts/Flipper.cs
+++ Assets/Scripts/Flipper.cs
@@ -5,13 +5,27 @@ using UnityEngine.InputSystem;
 
 public class Flipper : MonoBehaviour
 {
-    private Animator animator;
-    private PlayerInput input;
+    //private Animator animator;
+    //private PlayerInput input;
     //[SerializeField] AnimationClip flip;
 
-    public void OnFLip(InputAction input)
+    //public void OnFLip(InputAction input)
+    //{
+    //    animator.SetTrigger("Flip");
+    //}
+
+    public float restPosition = 0f;
+    public float pressedPosition = 45f;
+    public float hitStrength = 10000f;
+    public float flipperDamper = 150f;
+    public string inputName;
+
+    private void Update()
     {
-        animator.SetTrigger("Flip");
+        if(Input.GetAxis(inputName)==1)
+        {
+
+        }
     }
 }
 
--- Assets/Scripts/Release.cs
+++ Assets/Scripts/Release.cs
@@ -6,13 +6,10 @@ public class Release : MonoBehaviour
 {
     private void Update()
     {
+        //Deletes the cage the ball is held in when mouse is clicked so that it can drop
         if (Input.GetMouseButtonDown(0))
         {
             Destroy(gameObject);
         }
     }
-
-
-
-
 }
